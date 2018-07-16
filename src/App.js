f940f1c1ea4f482edce96a52cded03a980c50f26
FRCB <france.bushman@gmail.com>
2018-07-16 15:45:50 -0600

    boiler plate (structure and route)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter, withRouter } from 'react-router-dom';
+import routes from './routes';
+import Menu from './components/fixed/Menu';
+import Header from './components/fixed/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <Header />
+          <Menu />
+          {routes}
+        </div>
+      </HashRouter>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/user/Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+export default class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+
+    }
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
