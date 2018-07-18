a62c4464fc13825ae636c55d32a410d0f94bac1a
Seth Adamson <seth.c.adamson@gmail.com>
2018-07-18 15:46:12 -0600

    reducer updates

--- src/components/fixed/Header.js
+++ src/components/fixed/Header.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import chef from '../../media/chef-icon-13717.png';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+position: fixed;
+margin: 4vw;
+right: 0;
+border-radius: 50%;
+height: 70px;
+width: 70px;
+z-index: 100;
+&:hover {
+    -webkit-transform:scale(1.08);
+    -moz-transform:scale(1.08);
+    -ms-transform:scale(1.08);
+    -o-transform:scale(1.08);
+    transform: scale(1.08);
+}
+`
 
 
 class Header extends Component {
@@ -12,15 +31,21 @@ class Header extends Component {
 
     }
 
-
     render() {
-
+        let {user} = this.props;
+        console.log(user);
         return (
             <div>
-                
+                <Logo src={chef} />
             </div>
         )
     }
 }
 
-export default connect(mapStateToProps)()
+function mapStateToProps(state){
+    return {
+        user: state.user,
+    }
+};
+
+export default connect(mapStateToProps)(Header);
