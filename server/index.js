6da037b79c9177dd1370fd8a3d009d793c2f2f43
Avery Slight <averyslight99@gmail.com>
2018-07-18 15:52:16 -0600

    controller functionality done & endpoints working

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ const app = express();
 
 //--------------DotEnv----------//
 
-
-
 const {
     SERVER_PORT,
     SESSION_SECRET,
@@ -41,7 +39,7 @@ app.use(checkUserSession)
 app.post('/api/login', ctrl.loginUser)
 app.post('/api/register', ctrl.registerUser)
 app.get('/api/recipes', ctrl.allRecipes)
-app.post('/api/recipe', ctrl.newRecipe)
+app.post('/api/addrecipe', ctrl.newRecipe)
 app.put('/api/recipes/:id', ctrl.updateRecipe)
 app.delete('/api/recipes/:id', ctrl.deleteRecipe)
 
