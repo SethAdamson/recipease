4b38ba669ada28cd24d255a0945eac8ad21119d5
Seth Adamson <seth.c.adamson@gmail.com>
2018-07-20 10:55:01 -0600

    recipe/recipelist

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { getRecipes } from './../../ducks/reducer'
 import Recipe from './Recipe'
+import styled from 'styled-components';
+
+const Parent = styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 80vw;
+justify-content: space-around;
+margin: 0 auto;
+`
+
+
 class RecipeList extends Component {
     constructor() {
         super();
@@ -16,6 +27,7 @@ class RecipeList extends Component {
             this.setState({ recipes: this.props.recipes })
         }
     }
+
     render() {
         let allRecipes = this.state.recipes.map(e => {
 
@@ -24,16 +36,15 @@ class RecipeList extends Component {
                     rating={e.rating}
                     name={e.name}
                     img={e.img}
-                    rating={e.rating}
                     key={e.recipeid}
                 />
 
             )
         })
         return (
-            <div>
+            <Parent>
                 {allRecipes}
-            </div>
+            </Parent>
         )
     }
 }
