ef674a1070572b26a416762ff73ab2423d049b9e
Avery Slight <averyslight99@gmail.com>
2018-08-02 16:36:46 -0600

    corrected tests

--- src/components/user/loginTests/avery.test.js
+++ src/components/user/loginTests/avery.test.js
@@ -3,25 +3,12 @@ var state = {
     email: '',
     password: '',
     error: '',
-    loggedIn: '',
-    users: [{
-        userID: 0,
-        email: '',
-        password: ''
-    }],
-    userIDList: []
+    loggedIn: ''
 }
 var count = 1;
 
 function login(email, password) {
     state = { email: email, password: password }
-    for (let i = 0; i < this.state.users.length; i++) {
-        if (email === users[i].email && password === users[i].password) {
-            return 'logged in'
-        } else {
-            return 'incorrect credentials. try again.'
-        }
-    }
 }
 function logout() {
     state = {
@@ -29,22 +16,18 @@ function logout() {
         email: '',
         password: '',
         error: '',
-        loggedIn: ''
+        loggedIn: '',
+        users: []
     }
 }
-function register(email, password) {
-    const { userID, users, userIDList } = state
-    userID++;
-    users.push(userID, email, password)
-    userIDList.push(userID);
-    // this.state.userCount++;
+function register() {
+    state.users.push(count)
+    count++
 }
 function clearUsers() {
     state.users = []
 }
-function checkUser(email, password) {
 
-}
 test('userID should be 0', () => {
     expect(state.userID).toEqual(0);
 })
@@ -61,19 +44,13 @@ test('Logout should clear the state object', () => {
     expect(state.error).toEqual('')
     expect(state.loggedIn).toEqual('')
 })
-test('register should push a new user object to the users object in state', () => {
-    register('avery', 'slight')
-    expect(state.users[0].userID).toEqual(1)
-    expect(state.users[0].email).toEqual('avery')
-    expect(state.users[0].password).toEqual('slight')
-})
 test('register should increment userID and push it to users', () => {
     register()
     register()
     register()
     register()
-    expect(state.userIDList.length).toEqual(3)
-    expect(state.userID).toEqual(4)
+    expect(state.users.length).toEqual(4)
+    expect(count).toEqual(5)
 })
 test('clearusers should set users to 0', () => {
     clearUsers()
