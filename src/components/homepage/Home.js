22d972cba5043721e2669af4d2943d42c7e38b98
FRCB <france.bushman@gmail.com>
2018-08-29 19:08:17 -0400

    modifs

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -56,17 +56,10 @@ width: 100%;
 
 const HeaderImg = styled.div`
 img {
-<<<<<<< HEAD
-    display: none;
-
-    @media (min-width: 315px) and (max-width: 480px) {
-    display: initial;
-=======
 display: none;
 
 @media (min-width: 315px) and (max-width: 480px) {
-    display: block;
->>>>>>> master
+    display: initial;
     position: relative;
     width: 100%;
     height: 100%;
